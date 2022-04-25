f2b5d52610ffd0883c7b1472163c2dd02b95f650
Ruslan Khait <rusty6916@gmail.com>
2022-04-25 16:26:40 -0400

    commit tree a939a8188640d5eb8fb92d993407d5d622ff9224

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+#function greetProgrammer() {
+#  console.log("Hello, programmer!");
+#}
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil  
+    end
+    return num/2  
+end
+
+x = greet("Ruslan")
+puts x
